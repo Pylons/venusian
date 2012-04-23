0d9a267c30426d395ed01a6a7b93933645e8a873
Chris McDonough <chrism@plope.com>
2012-04-23 13:01:37 -0400

    fix on 2.4 and 2.5 platforms

--- venusian/tests/fixtures/import_and_scan/one.py
+++ venusian/tests/fixtures/import_and_scan/one.py
@@ -1,5 +1,5 @@
 from venusian.tests.fixtures import decorator
-from .two import twofunction # should not be scanned
+from venusian.tests.fixtures.import_and_scan.two import twofunction # should not be scanned
 
 @decorator(function=True)
 def onefunction(request): # pragma: no cover
