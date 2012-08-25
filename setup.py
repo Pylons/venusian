892d07199d437248e20db5838d75cecda8c01ad5
Chris McDonough <chrism@plope.com>
2012-08-25 04:06:36 -0400

    prep for 1.0a7

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ tests_extras = ['nose', 'nose-exclude', 'coverage']
 docs_extras = ['Sphinx', 'repoze.sphinx.autointerface']
 
 setup(name='venusian',
-      version='1.0a6',
+      version='1.0a7',
       description='A library for deferring decorator actions',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
