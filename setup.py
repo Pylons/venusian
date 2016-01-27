3949113743f5c9e1df3c245f8062e5180c1401da
faassen <faassen@startifact.com>
2016-01-27 11:16:19 +0100

    Some venusifork release stuff wasn't actually properly checked in.

--- setup.py
+++ setup.py
@@ -33,8 +33,8 @@ tests_extras = [
     ]
 docs_extras = ['Sphinx', 'repoze.sphinx.autointerface']
 
-setup(name='venusian',
-      version='1.0',
+setup(name='venusifork',
+      version='2.0a3',
       description='A library for deferring decorator actions',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
